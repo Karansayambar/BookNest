ba64995ee158eb0bf3d8951ef026dfe301e90cf4
SayambarKaran <karan.subhashsayambar@quadbtech.com>
2025-02-27 13:53:50 +0530

    frontend code updated

--- server/app.js
+++ server/app.js
@@ -22,7 +22,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
