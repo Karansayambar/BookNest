e6cf38d06a306b6b3f8c0270ccd1565aa5580df3
SayambarKaran <karan.subhashsayambar@quadbtech.com>
2025-02-27 14:00:48 +0530

    backend  code updated

--- server/app.js
+++ server/app.js
@@ -24,7 +24,6 @@ app.use(
 );
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Routes
